1a58d61b32a5a60fe001b2f5f3bbbf3c7f0efcb6
tryles <trevorryles02@gmail.com>
2023-12-18 18:06:16 -0800

    APIs implemented

--- server/index.js
+++ server/index.js
@@ -1,11 +1,58 @@
 import express from "express";
 import cors from "cors";
-
+import pool from "./pool.js"
 const app = express();
 // create a server and listen on port 3000
 app.use(cors());
-app.get("/", (req, res) => {
-    res.json({"hoes":"here"});
+app.use(express.json());
+app.get("/todos", async(req, res) => {
+    try{
+        const todos = await pool.query("SELECT * FROM todo");
+        res.json(todos.rows);
+    }catch(err){
+        console.log(err);
+    }
+});
+
+app.get("/todos/:id", async(req, res) => {
+    try{
+        const id = req.params.id;
+        const todos = await pool.query("SELECT * FROM todo WHERE todo_id=$1", [id]);
+        res.json(todos.rows[0]);
+    }catch(err){
+        console.log(err);
+    }
+});
+
+app.post("/todos", async(req, res) => {
+    try{
+        let {description} = req.body;
+        const todo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *", [description]);
+        res.json(todo.rows[0]);
+    }catch(err){
+        console.log(err);
+    }
+});
+
+app.delete("/todos/:id", async(req, res) => {
+    try{
+        const id = req.params.id;
+        const todo = await pool.query("DELETE FROM todo WHERE todo_id=$1 RETURNING *", [id]);
+        res.json(todo.rows[0]);
+    }catch(err){
+        console.log(err);
+    }
+});
+
+app.put("/todos/:id", async(req, res) => {
+    try{
+        const desc = req.body.description;
+        const id = req.params.id;
+        const todo = await pool.query("UPDATE todo SET description=$1 WHERE todo_id=$2 RETURNING *", [desc, id]);
+        res.json(todo.rows[0]);
+    }catch(err){
+        console.log(err);
+    }
 });
 
 app.listen(5000, () => {
---
+++ server/pool.js
@@ -0,0 +1,14 @@
+import pkg from "pg";
+const {Pool} = pkg;
+
+// Create a PostgreSQL connection pool
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'perntodo',
+    password: 'password',
+    port: 5432, // Default PostgreSQL port
+    idleTimeoutMillis: 30000, // Time a connection can be idle before being closed
+});
+
+export default pool;
